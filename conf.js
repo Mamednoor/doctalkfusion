907e5665c19e7c7d6ff325de232812f36fcb03e2
Mamednoor <mamed.gohabur@gmail.com>
2020-08-27 01:07:31 +0200

    using CLEARDB_DATABASE_URL env variable for db settings to deploy in heroku

--- conf.js
+++ conf.js
@@ -1,14 +1,18 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST, 
-  user: process.env.DB_USER, 
-  password: process.env.DB_PASS, 
-  database: process.env.DB_NAME
-})
+const dbSetting = typeof process.env.CLEARDB_DATABASE_URL === 'string'
+  ? process.env.CLEARDB_DATABASE_URL
+  : {
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASS, 
+    database: process.env.DB_NAME
+  }
 
-const d = new Date()
-const date = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate()
-console.log("server is up", date)
+  const d = new Date()
+  const date = d.getDate() + '-' + (d.getMonth() + 1) + '-' + d.getFullYear()
+  console.log("server is up", date)
+  
+  const connection = mysql.createConnection(dbSetting)
 
 module.exports = connection
